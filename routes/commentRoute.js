4ce7f43b3035a4d35e2dff766d43acbcba13daa5
samyog7901 <samyogkhadka247@gmail.com>
2025-02-19 19:58:13 +0545

    Now Ok

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -4,7 +4,7 @@ const catchError = require("../services/catchError")
 
 const router = require("express").Router()
 
-router.route("/comment").post(isAuthenticated,catchError(addComment))
+router.route("/comment").post(catchError(addComment))
 router.route("/deleteComment/:id").get(isAuthenticated,catchError(deleteComment))
 
 module.exports = router  //export the router to use in other files
